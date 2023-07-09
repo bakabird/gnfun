ab581c3f6e91646e256ec8f900f4ce89e4cf5fb1
rddInSZ <bakabird@sina.cn>
2023-07-09 20:57:34 +0800

    0.0.5 打包

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import { FreeList } from "./Collection/FreeList";
+export { FreeList } from "./Collection/FreeList";
 import CountdownRunner from "./Runner/CountdownRunner";
 import RoundRunner from "./Runner/RoundRunner";
 
-export default {
-    FreeList,
-    RoundRunner,
-    CountdownRunner,
-}
+export { CountdownRunner };
+export { RoundRunner };
